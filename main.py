8fa6baf29ef76c08dfeba9d249a9824e08f2ed86
FreshBoy89 <kendrickvella57@gmail.com>
2025-08-20 19:36:11 -0600

    version 0.1

---
+++ main.py
@@ -0,0 +1,55 @@
+import os 
+def creation_carpet(name):
+    try:
+        os.mkdir(name)
+        print(f"The directory {name} is created sucessfully")
+    except FileExistsError:
+        print(f"The directory {name} exists")
+def creation_file(name,archive,extension):
+    name_archive_extension = f"{archive}.{extension}"
+    try:
+        os.path.join(name,name_archive_extension)
+        print(f"The archive {name_archive_extension} is created")
+    except FileExistsError:
+        print(f"Error the file is not created")
+#def replace_dates():
+def delete_carpet(name_carpet):
+    try:
+        os.rmdir(name_carpet)
+        print(f"The directory {name_carpet} has deleted.")
+    except FileNotFoundError:
+        print(f"The directory {name_carpet} does not exist ")
+    except OSError:
+        print(f"The directory {name_carpet} is not empty or cannot be deleted")
+def main():
+    try:    
+        while True:
+            print("[1] Creation carpet")
+            print("[2] Create file")
+            print("[3] Rename Carpet")
+            print("[4] Delete carpet")
+            print("[5] Exit")
+            option = int(input(">"))
+            if option == 1:
+                name = input("Enter the name of the carpet ->")
+                creation_carpet(name)
+            elif option == 2:
+                name = input("Enter the name of the carpet ->")
+                archive = input("Enter the name of the archive ->")
+                extend = input("Enter the extend archive ->")
+                creation_file(name,archive,extend)
+            elif option == 3:
+                break
+            elif option == 4:
+                name_carpet = input("Enter the name of the carpet ->")
+                delete_carpet(name_carpet)
+                break
+            elif option == 5:
+                print("Exit........")
+                break
+    except ValueError:
+        print("Error: You must be enter a number (1-5).")
+    except Exception as e:
+        print(f"Unexpected error : {e}")
+if __name__ == "__main__":
+    main()
